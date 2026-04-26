224f48bc34d75018dcd93cd8f18290058fa808f6
Drew Farley <old2news@gmail.com>
2026-04-25 18:26:15 -0600

    various

--- dataEnum.py
+++ dataEnum.py
@@ -1,25 +1,28 @@
 # 5_0_154 is probably tree base
 tileCategories = [
     'ore',
-    'common',   # Dirt, stone
     'gem',
-    'biome',    # Biome specific: mud, hive block
-    'grass',
-    'light',    # Torch, campfire
-    'furniture',
-    'pot',
-    'crafting_station',
+    'small_plants',
+    'large_plants',
     'banner',
-    'statue',
-    'wiring',   # Any item that uses wiring
-    'decoration',   # Decorative tiles that don't drop anything
-    'boss_summon',  # Plantera bulb, crimson heart, etc.
-    'boss_trophy',
-    'painting',
+    'loot_pot',
+    'crafting_station',
+    'relic',
     'tree',
     'fishing_crate',
-    'platform', # And planter boxes
+    'platform',  # And planter boxes
     'pylon',
-    'buff_station', # Cake, ammo box
+    'statue',
+    'lighting',    # Torch, campfire
+    'boss_summon',  # Plantera bulb, crimson heart, etc.
+    'painting',
+    'buff_station',  # Cake, ammo box
+    'mechanisms',   # Any item that uses wiring
+    'boulder',
+    'rubble',   # Decorative tiles that don't drop anything
     'other'
+    # The below is unused
+    'common',   # Dirt, stone
+    'biome',    # Biome specific: mud, hive block
+    'furniture',
 ]
--- snipCapture.py
+++ snipCapture.py
@@ -5,9 +5,9 @@ import numpy as np
 import os
 import cv2
 from functools import lru_cache
-from pathlib import Path
+# from pathlib import Path
 import random
-import time
+# import time
 import math
 
 
@@ -17,6 +17,7 @@ trainingPath = './training'
 @lru_cache(maxsize=3000)
 def getImage(path):
     return Image.open(path)
+
 
 @lru_cache(maxsize=3000)
 def getImageArray(path):
@@ -29,7 +30,55 @@ def areNpArraysSimilar(arr1, arr2):
     return diff < 2
 
 
-def classifyImage(nparray, tileInfo):
+def getIDcategory(numID: int) -> str:
+    elif numID in [6, 7, 8, 9, 22, 37, 56, 58, 107, 108, 111, 166, 167, 168, 169, 204, 211, 221, 222, 223, 408]:
+        return 'ore'
+    elif numID in [178]:
+        return 'gem'
+    elif numID in [3, 24, 61, 71, 73, 74, 110, 113, 201, 637, 703]:
+        return 'small_plants'
+    elif numID in [227, 233, 530, 651, 652, 705]:
+        return 'large_plants'
+    elif numID in [91, 465]:
+        return 'banner'
+    elif numID in [28, 653]:
+        return 'loot_pot'
+    elif numID in [16, 17, 18, 13, 26, 77, 86, 94, 96, 101, 106, 114, 125, 133, 134, 172, 207, 217, 218, 219, 220, 228, 237, 243, 247, 283, 300, 301, 302, 303, 304, 305, 306, 307, 308, 355, 412, 499, 622, 642, 695, 699]:
+        return 'crafting_station'
+    elif numID in [617]:
+        return 'relic'
+    elif numID in [5, 20, 72, 323, 571, 583, 584, 585, 586, 587, 588, 589, 590, 595, 596, 615, 616, 634]:
+        # And saplings
+        return 'tree'
+    elif numID in [376]:
+        return 'fishing_crate'
+    elif numID in [19, 380, 427, 435, 436, 437, 438, 439]:
+        # And planter boxes (id=380)
+        return 'platform'
+    elif numID in [597]:
+        return 'pylon'
+    elif numID in [105, 337, 349, 531]:
+        return 'statue'
+    elif numID in [33, 49, 100, 174, 372, 646] or numID in [100, 173] or numID in [42, 92, 93, 100, 390, 564, 34, 95, 98] or numID in [4, 93] or numID in [35, 126, 149, 215, 405, 592]:
+        return 'lighting'
+    elif numID in [31, 129, 231, 237, 238]:
+        # id 129 gelatin crystal and crystal shards
+        return 'boss_summon'
+    elif numID in [240, 241, 242, 245, 246]:
+        return 'painting'
+    elif numID in [287, 354, 377, 464, 621]:
+        return 'buff_station'
+    elif numID in [131, 132, 135, 136, 137, 141, 142, 143, 144, 210, 216, 235, 314, 335, 338, 411, 419, 420, 421, 422, 423, 424, 425, 428, 429, 442, 443, 445]:
+        return 'mechanism'
+    elif numID in [138, 664, 665, 711, 712, 713, 714, 715, 716]:
+        return 'boulder'
+    elif numID in [185, 186, 187, 647, 648, 649, 650, 693, 694, 704, 705, ]:
+        return 'rubble'
+    else:
+        return 'other'
+
+
+def classifyImage(nparray: np.ndarray, tileInfo) -> str:
     # potentialClasses = []   # A tile might have water and a wall, so we would want to randomly decide which one so the AI learns for both cases
     # ABOVE IS DEPRECATED
     # Return in this order: unknown, tile, liquid, wall, air
@@ -37,7 +86,7 @@ def classifyImage(nparray, tileInfo):
     brightness = sum(lighting) / 3 / 255
     isDark = brightness < 0.1   # Tile is too dark for a human to distinguish
     tileID = tileInfo['type']
-    if tileInfo['hasTile'] != False:
+    if tileInfo['hasTile'] is not False:
         if isDark: return 'tile/unknown'
         if tileID in [3, 4, 5, 13, 24, 28, 50, 61, 82, 83, 84, 91, 105, 110, 135, 137, 144, 178, 201, 209, 215, 235, 239, 254, 323, 376, 419, 420, 423, 429, 443, 583, 584, 585, 586, 587, 588, 589, 596, 597, 616, 634, 653, 663]:
             # Tile has important semantically meaningful subID
@@ -48,7 +97,7 @@ def classifyImage(nparray, tileInfo):
             return f'tile/{tileID}_{tilefX}_{tilefY}'
         else:
             return f'tile/{tileID}'
-    if tileInfo['liquidAmount'] != 0 and (tileInfo['isSolid'] == False or tileInfo['isActuated'] == True):
+    if tileInfo['liquidAmount'] != 0 and (tileInfo['isSolid'] is True or tileInfo['isActuated'] is True):
         # Liquid
         if isDark: return 'liquid/unknown'
         liquidType = tileInfo['liquidType']
@@ -75,7 +124,7 @@ def classifyImage(nparray, tileInfo):
         return f'tile_{tileID}_{tilefX}_{tilefY}'
 
 
-def saveClassifyMiddleSnip(img, captureData):
+def saveClassifyMiddleSnip(img: np.ndarray, captureData: dict) -> None:
     #img[284:484, 583:783]
     middleImg = img[284:484, 583:783]
     x_offset = int(15 - (captureData['ScreenPosX'] % 16))
@@ -86,42 +135,29 @@ def saveClassifyMiddleSnip(img, captureData):
     cv2.imwrite(f'{yPath}/{len(os.listdir(yPath))}.png', middleImg)
 
 
-def snipImageAndSaveClassified(img, captureData, endMessage=None, nogoZone=None):
-    saveClassifyMiddleSnip(img, captureData)
-    if nogoZone is None:
-        # This nogoZone is formatted for a UI scale of 80%
-        cursorPos = captureData['CursorPos']
-        nogoZone = [
-            ((1135, 13), (1330, 63)),   # Health
-            ((1335, 16), (1360, 197)),  # Mana
-            ((17, 0), (357, 56)),       # Hotbar
-            ((25, 60), (348, 134)),     # Two rows of buffs. Four rows goes to y=211
-            ((660, 405), (707,465)),    # The player
-            ((cursorPos[0], cursorPos[1]), (cursorPos[0] + 20, cursorPos[1] + 20))  # The cursor
-        ]
-    pixels = img[:]
+def tileImage(img: np.ndarray, nogoZone=None) -> np.ndarray:
     height, width, channels = img.shape
     tileWidth, tileHeight = floor(width / 16) - 1, floor(height / 16) - 1
-    tileImages = [[0 for x in range(int(tileWidth/3))] for y in range(int(tileHeight/3))]
-    tileData = captureData['TileData']
-    x_adjust = 16 - int(captureData['ScreenPosX'])%16
-    y_adjust = 16 - int(captureData['ScreenPosY'])%16
-    for x in range(int(tileWidth/3)):
-        for y in range(int(tileHeight/3)):
-            corners = ((16*x + x_adjust, 16*y + y_adjust), (16*(x+1) + x_adjust - 1, 16*y + y_adjust), (16*x + x_adjust, 16*(y+1) + y_adjust - 1), (16*(x+1) + x_adjust - 1, 16*(y+1) + y_adjust - 1))
-            corners = ((48*x, 48*y), (48*(x+1), 48*y), (48*x, 48*(y+1)), (48*(x+1), 48*(y+1)))
+    tileImages = [[0 for x in range(tileWidth - 2)] for y in range(tileHeight - 2)]
+    for x in range(tileWidth - 2):
+        for y in range(tileHeight - 2):
             isInNogoZone = False
-            for zone in nogoZone:
-                if isInNogoZone: break;
-                for corner in corners:
-                    if corner[0] > zone[0][0] and corner[0] < zone[1][0] and corner[1] > zone[0][1] and corner[1] < zone[1][1]:
-                        isInNogoZone = True
-                        break;
+            if nogoZone is not None:
+                corners = ((16*x, 16*y), (16*(x+3), 16*y), (16*x, 16*(y+3)), (16*(x+3), 16*(y+3)))
+                for zone in nogoZone:
+                    if isInNogoZone: break
+                    for corner in corners:
+                        if corner[0] > zone[0][0] and corner[0] < zone[1][0] and corner[1] > zone[0][1] and corner[1] < zone[1][1]:
+                            isInNogoZone = True
+                            break
             if isInNogoZone: continue
-            #newtilePixelArray = pixels[16*y + y_adjust:16*(y+1) + y_adjust, 16*x + x_adjust:16*(x+1) + x_adjust, :]
-            newtilePixelArray = pixels[48*y:48*(y+1), 48*x:48*(x+1), :]
+            #newtilePixelArray = img[16*y + y_adjust:16*(y+1) + y_adjust, 16*x + x_adjust:16*(x+1) + x_adjust, :]
+            newtilePixelArray = img[16*y:16*(y+3), 16*x:16*(x+3), :]
             tileImages[y][x] = newtilePixelArray
+    return tileImages
 
+
+def saveTiles(tileImages: np.ndarray, tileData: dict) -> None:
     classifiedImageCounts = dict()
     # classifiedImageCounts is used because os.listdir takes a lot of time
     for mainPath in os.listdir(trainingPath):
@@ -134,10 +170,10 @@ def snipImageAndSaveClassified(img, captureData, endMessage=None, nogoZone=None)
             if not isinstance(image, np.ndarray): continue
             # if np.mean(image) < 25: continue    # Image is too dark to tell what it is
             # ABOVE IS DEPRECATED: using tile lighting from tModLoader to determine if tile is too dark
-            tileInfo = tileData[3*x][3*y]
+            tileInfo = tileData[x+1][y+1]
             classified = classifyImage(image, tileInfo)
             path = f'{trainingPath}/{classified}'
-            imgAlreadyInSet = False 
+            imgAlreadyInSet = False
             nameID = classifiedImageCounts.get(classified)
             if nameID is None:
                 os.mkdir(path)
@@ -148,4 +184,25 @@ def snipImageAndSaveClassified(img, captureData, endMessage=None, nogoZone=None)
                 continue
             classifiedImageCounts[classified] = nameID + 1
             cv2.imwrite(f'{path}/{nameID}.png', image)
+
+
+def snipImageAndSaveClassified(img: np.ndarray, captureData, endMessage=None, nogoZone=None) -> None:
+    saveClassifyMiddleSnip(img, captureData)
+    if nogoZone is None:
+        # This nogoZone is formatted for a UI scale of 80%
+        cursorPos = captureData['CursorPos']
+        nogoZone = [
+            ((1135, 13), (1330, 63)),   # Health
+            ((1335, 16), (1360, 197)),  # Mana
+            ((17, 0), (357, 56)),       # Hotbar
+            ((25, 60), (348, 134)),     # Two rows of buffs. Four rows goes to y=211
+            ((660, 405), (707, 465)),    # The player
+            ((cursorPos[0], cursorPos[1]), (cursorPos[0] + 20, cursorPos[1] + 20))  # The cursor
+        ]
+    #tileImages = [[0 for x in range(tileWidth - 2)] for y in range(tileHeight - 2)]
+    #x_adjust = 16 - int(captureData['ScreenPosX'])%16
+    #y_adjust = 16 - int(captureData['ScreenPosY'])%16
+    tileImages = tileImage(img, nogoZone)
+    saveTiles(tileImages, captureData['TileData'])
+
     if endMessage is not None: print(endMessage)
